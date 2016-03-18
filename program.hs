047532cf4450d6767f277d0c377902149a9af628
fzoozai <fabian.soosaithasan@gmail.com>
2016-03-18 11:23:32 +0100

    Implement filter function

--- program.hs
+++ program.hs
@@ -18,8 +18,19 @@ watch n = show n ++ "o'clock"
 add1 :: Int -> Int
 add1 x = x + 1
 
+
+
+{-Map-}
+
 {-map add1 [1,2,3] delivers [2,3,4]-}
 
 {-map (max 3) [1,2,3,4,5] delivers [3,3,3,4,5]. Map uses the function max and combines it with the array.
 So it compares every single element in the array and if it is greater then den max value (3) it keeps
 it value and position. If it is less then the max value so replace it with the max value-}
+
+{-Filter-}
+
+{-filter (>5) [1..10] delivers [6,7,8,9,10]-}
+
+{-filter takes a function in a list and gives us a list of those elements for which the function returns True-}
+
